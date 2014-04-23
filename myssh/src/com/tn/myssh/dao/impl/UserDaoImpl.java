8e8427eb43d6cef8cecaf4ce7adb9b32fbb22acf
shihensh <taolihao@gmail.com>
2014-04-23 17:45:32 +0800

    4-23

--- myssh/src/com/tn/myssh/dao/impl/UserDaoImpl.java
+++ myssh/src/com/tn/myssh/dao/impl/UserDaoImpl.java
@@ -1,7 +1,22 @@
 package com.tn.myssh.dao.impl;
 
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import com.tn.myssh.pojo.User;
+
+
 public class UserDaoImpl extends BaseDaoImpl {
 
-	
+	public User getUserByNameAndPwd(String username,String password){
+		
+		String hql="from User where username=:usn and password=:pwd";
+		Session session = this.getSession();
+		Query query = session.createQuery(hql);
+		query.setParameter("usn", username);
+		query.setParameter("pwd", password);
+		return (User)query.uniqueResult();
+		
+	}
 }
