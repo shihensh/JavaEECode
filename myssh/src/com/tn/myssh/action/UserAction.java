8d8b4257ed4e1d54a6d70f999503e33525dc0fb0
shihensh <taolihao@gmail.com>
2014-04-29 14:02:41 +0800

    4.29

--- myssh/src/com/tn/myssh/action/UserAction.java
+++ myssh/src/com/tn/myssh/action/UserAction.java
@@ -11,31 +11,22 @@ public class UserAction {
 	private User user;
 	private String username;
 	private String password;
-	
 
 	public String getUsername() {
 		return username;
 	}
 
-
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 
 	public void setUserService(UserService userService) {
 		this.userService = userService;
@@ -44,8 +35,6 @@ public class UserAction {
 
 	/**
 	 * µÇÂ½
-	 * @param username
-	 * @param password
 	 * @return
 	 */
 	public String login(){
@@ -73,4 +62,7 @@ public class UserAction {
 		ActionContext.getContext().getSession().put("user", user);
 		return "succeed";
 	}
+	
+	
+	
 }
